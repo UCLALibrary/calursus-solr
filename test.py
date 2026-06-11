d90a9697e61cf7fbc4e97d4f367b37e9ed7e7daf
Andy Wallace <andrewwallace@library.ucla.edu>
2026-06-10 20:25:17 -0700

    DLSR-5 use solr.ICUCollationField for unicode-aware, alphanumeric sort (#22)
    * Move analysis-extras loading to solrconfig.xml
    * use *_tsort instead of *_ssort for ICUCollationField
    
    This leaves existing fields unchanged for easier migration, and the 't' suffix is more appropriate to the function (indicating analysis as text rather than a basic string symbol). It leaves _ssort available for any future fields (like identifiers) we might want to sort by.
    
    * add default for SOLR_TEST_URL to test.py, which corresponds to the address when running solr_test in docker and the test suite itself outside of docker. Also fixes some commenting-out I'd done for debugging and accidentally left commented.
    * fixes docker-compose.yml's services.test.environment such that SOLR_TEST_URL points to the internal port of solr_test (8983), rather than the external one (8985)
    * adds healthcheck and depends_on...condition: service_health to docker-compose.yml, so that `docker-compose run test` will wait for solr_test to be available before running.
    * fixes ci.yml to be triggered by changes to the branch `master`, since we never updated this repo to use `main`. Keeps the trigger from `main`, so we can make that change in the future.
    * add test instructions to README
    * ignore punctuation in ICUCollationField sort
    * Use order_desc variables for Z-A desscending order tests

---
+++ test.py
@@ -0,0 +1,122 @@
+import os
+from random import shuffle
+from unittest import TestCase
+
+import requests
+
+SOLR_URL = os.getenv("SOLR_TEST_URL", "http://localhost:8985/solr/ursus")
+
+
+class TestSort(TestCase):
+    def setUp(self):
+        requests.post(
+            f"{SOLR_URL}/update?commit=true",
+            json={"delete": {"query": "*:*"}},
+            headers={"Content-Type": "application/json"},
+        )
+
+    def tearDown(self):
+        requests.post(
+            f"{SOLR_URL}/update?commit=true",
+            json={"delete": {"query": "*:*"}},
+            headers={"Content-Type": "application/json"},
+        )
+
+    def test_sorts_non_latin(self):
+        docs = [
+            {"id": "1", "title_tsort": "x"},
+            {"id": "2", "title_tsort": "y"},
+            {"id": "3", "title_tsort": "z"},
+            {"id": "4", "title_tsort": "あ(a)"},
+            {"id": "5", "title_tsort": "い(i)"},
+            {"id": "6", "title_tsort": "う(u)"},
+        ]
+        shuffle(docs)
+
+        # Index documents
+        requests.post(
+            f"{SOLR_URL}/update?commit=true",
+            json=docs,
+            headers={"Content-Type": "application/json"},
+        )
+
+        # Query documents, A–Z
+        response_asc = requests.get(
+            f"{SOLR_URL}/select",
+            params={"q": "*:*", "sort": "title_tsort asc"},
+        )
+
+        order_asc = [doc["id"] for doc in response_asc.json()["response"]["docs"]]
+        self.assertEqual(order_asc, ["1", "2", "3", "4", "5", "6"])
+
+        # Query documents, Z–A
+        response_desc = requests.get(
+            f"{SOLR_URL}/select",
+            params={"q": "*:*", "sort": "title_tsort desc"},
+        )
+        order_desc = [doc["id"] for doc in response_desc.json()["response"]["docs"]]
+        assert order_desc == ["6", "5", "4", "3", "2", "1"]
+
+    def test_sorts_numeric(self):
+        docs = [
+            {"id": "1", "title_tsort": "thing 1"},
+            {"id": "2", "title_tsort": "thing 9"},
+            {"id": "3", "title_tsort": "thing 10"},
+        ]
+        shuffle(docs)
+
+        # Index documents
+        requests.post(
+            f"{SOLR_URL}/update?commit=true",
+            json=docs,
+            headers={"Content-Type": "application/json"},
+        )
+
+        # Query documents, A–Z
+        response_asc = requests.get(
+            f"{SOLR_URL}/select",
+            params={"q": "*:*", "sort": "title_tsort asc"},
+        )
+
+        order_asc = [doc["id"] for doc in response_asc.json()["response"]["docs"]]
+        self.assertEqual(order_asc, ["1", "2", "3"])
+
+        # Query documents, Z–A
+        response_desc = requests.get(
+            f"{SOLR_URL}/select",
+            params={"q": "*:*", "sort": "title_tsort desc"},
+        )
+        order_desc = [doc["id"] for doc in response_desc.json()["response"]["docs"]]
+        assert order_desc == ["3", "2", "1"]
+
+    def test_ignores_punctuation(self):
+        docs = [
+            {"id": "1", "title_tsort": "a"},
+            {"id": "2", "title_tsort": "'''b"},
+            {"id": "3", "title_tsort": "c"},
+        ]
+        shuffle(docs)
+
+        # Index documents
+        requests.post(
+            f"{SOLR_URL}/update?commit=true",
+            json=docs,
+            headers={"Content-Type": "application/json"},
+        )
+
+        # Query documents, A–Z
+        response_asc = requests.get(
+            f"{SOLR_URL}/select",
+            params={"q": "*:*", "sort": "title_tsort asc"},
+        )
+
+        order_asc = [doc["id"] for doc in response_asc.json()["response"]["docs"]]
+        self.assertEqual(order_asc, ["1", "2", "3"])
+
+        # Query documents, Z–A
+        response_desc = requests.get(
+            f"{SOLR_URL}/select",
+            params={"q": "*:*", "sort": "title_tsort desc"},
+        )
+        order_desc = [doc["id"] for doc in response_desc.json()["response"]["docs"]]
+        assert order_desc == ["3", "2", "1"]
